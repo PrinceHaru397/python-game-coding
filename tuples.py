5cf00796d247b799b128f5300e6d1b2de478309e
PrinceHaru397 <kitadeharuki02@gmail.com>
2026-06-08 16:01:24 +0200

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,13 @@
+# Define the tuple
+t = (100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)
+
+# Print the tuple
+print(t)
+
+# Print all the values
+for i in t:
+    print(i)
+
+# Nested tuples
+matrix = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
+print(matrix)
